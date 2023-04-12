aeba4190226aa85a73f7574602919fdddc1ec81f
Rohan Singh <rohannsingh98765@gmail.com>
2023-04-12 15:13:13 +0530

    bg
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import { Routes ,Route } from "react-router-dom"
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
-    <div>App</div>
+      <>
+
+
+        <Box className="bg-[url(././assets/bgg.png)] bg-center bg-no-repeat bg-cover">
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+      </>
   )
 }
 
