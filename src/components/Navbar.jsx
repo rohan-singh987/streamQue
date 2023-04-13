fd68cf15fade121d8da506dfb9b2a0f937ce9ce6
Rohan Singh <rohannsingh98765@gmail.com>
2023-04-13 06:32:13 +0530

    feed
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./SearchBar";
 
 
 const Navbar = () => (
-  <Stack  sx={{ flexDirection: { sx: "column", md: "row" }, position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }} p={2}>
+  <Stack  sx={{ flexDirection: { sx: "column", md: "row" }, position:  "sticky", zIndex:10 , background: '#000', top: 0, justifyContent: "space-between" }} p={2}>
     <Link to="/" className="flex items-center">
         <span className="text-[#19A7CE] font-bold text-4xl">
             Stream
